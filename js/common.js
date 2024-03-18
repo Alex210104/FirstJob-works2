295ed9e5b0157c26b7a313f6c5c7405bba033d87
Alex210104 <shield210104@gmail.com>
2024-03-18 16:31:50 +0800

    first commit

---
+++ js/common.js
@@ -0,0 +1,55 @@
+document.querySelector("footer").innerHTML = `
+<div class="downbox">
+        <div class="wrapbox">
+          <ul class="downmenu">
+            <li><a href="#">關於金品</a></li>
+
+            <li><a href="#">產品介紹</a></li>
+
+            <li><a href="#">門市據點</a></li>
+
+            <li><a href="#">最新消息</a></li>
+
+            <li><a href="#">聯絡我們</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="endArea">
+        <div class="wrapbox row">
+          <div class="qrbox">
+            <div class="qrcode">
+              <img src="./img/qrcode.png" alt="" />
+              <h5 class="copyright">
+                <b>金品醬園剝皮辣椒</b>
+                <span style="color: #94772a">JINPIN</span>
+                <br />
+                <span>統一編號 : </span>
+                92095643
+                <br />
+                <span>連絡電話 : </span>
+                03-8762233 / 03-8761555
+                <br />
+                <span>傳真電話 : </span>
+                03-8764555
+                <br />
+                <span>連絡地址 : </span>
+                975花蓮縣鳳林鎮信義路198號
+              </h5>
+            </div>
+            <div class="g-6 rightBox b-box">
+              <!--row-->
+              <a href="https://www.facebook.com/jinpinchilly/" target="_blank"
+                ><i class="fa fa-facebook-official"></i
+              ></a>
+              <span>金品醬園剝皮辣椒 © 2016</span>
+              <span class="web"
+                >網站設計 |
+                <a href="http://www.ibest.com.tw" target="_blank"
+                  >愛貝斯</a
+                ></span
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+`;
